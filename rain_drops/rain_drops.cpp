2cf5fe9600b96e8d56a05ebce1126efbb4a3e68f
Michal Moudrý <moudry.michal@live.com>
2023-08-30 20:29:26 +0200

    Finished rain drops exercise

---
+++ rain_drops/rain_drops.cpp
@@ -0,0 +1,32 @@
+#include "rain_drops.h"
+#include <iostream>
+
+namespace raindrops {
+    std::string convert(int value)
+    {
+        if (value % 3 != 0 && value % 5 != 0 && value % 7 != 0)
+        {
+            return std::to_string(value);
+        }
+        std::string result = "";
+        if (value % 3 == 0)
+        {
+            result += "Pling";
+        }
+        if (value % 5 == 0)
+        {
+            result += "Plang";
+        }
+        if (value % 7 == 0)
+        {
+            result += "Plong";
+        }
+        return result;
+    }
+}
+
+int main() {
+    std::cout << raindrops::convert(1) << std::endl;
+    std::cout << raindrops::convert(3) << std::endl;
+    std::cout << raindrops::convert(105) << std::endl;
+}
---
+++ rain_drops/rain_drops.h
@@ -0,0 +1,10 @@
+#if !defined(RAINDROPS_H)
+#define RAINDROPS_H
+
+#include <string>
+
+namespace raindrops {
+    std::string convert(int value);
+}  // namespace raindrops
+
+#endif // RAINDROPS_H
