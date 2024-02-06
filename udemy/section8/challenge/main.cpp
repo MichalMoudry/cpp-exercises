2e83cd5678f679e14b210b9cab156d4a319c6307
Michal Moudrý <moudry.michal@live.com>
2024-02-06 12:58:47 +0100

    Finished section 8 challange

--- udemy/section8/challenge/main.cpp
+++ udemy/section8/challenge/main.cpp
@@ -6,6 +6,27 @@
 using namespace std;
 
 int main() {
+    int cents_number {0};
+    cout << "Enter an amount in cents: ";
+    cin >> cents_number;
 
+    int dollars {cents_number/100};
+    cout << "dollars: " << dollars << endl;
+    cents_number %= 100;
+
+    int quarters {cents_number/25};
+    cout << "quarters: " << quarters << endl;
+    cents_number %= 25;
+
+    int dimes {cents_number/10};
+    cout << "dimes: " << cents_number/10 << endl;
+    cents_number %= 10;
+
+    int nickles {cents_number/5};
+    cout << "nickles: " << nickles << endl;
+    cents_number %= 5;
+
+    int pennies {cents_number/1};
+    cout << "pennies: " << pennies << endl;
     return 0;
 }
