0a60043603a04c4487d1e246d363ed98930e2c19
Michal Moudrý <moudry.michal@live.com>
2024-04-25 17:00:17 +0200

    Finished one part of section 12

--- udemy/section12/dynamic_memory/main.cpp
+++ udemy/section12/dynamic_memory/main.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
-    cout << endl;
+    int *int_ptr {nullptr};
+    int_ptr = new int;
+    cout << int_ptr << endl;
+    delete int_ptr;
+
+    size_t size {0};
+    double *temp_ptr {nullptr};
+    cout << "How many temps? ";
+    cin >> size;
+    temp_ptr = new double[size]; // Store address of the first element
+    cout << temp_ptr << endl;
+    delete [] temp_ptr;
+
     return 0;
 }
