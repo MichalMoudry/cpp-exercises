9019d3f0501da23dab781cee0fb28d5ee6aad924
Michal Moudrý <moudry.michal@live.com>
2024-07-17 17:28:58 +0200

    finished section 19

--- udemy/section19/string_streams/main.cpp
+++ udemy/section19/string_streams/main.cpp
@@ -15,8 +15,33 @@ int main() {
         << std::setw(5) <<  num
         << std::setw(10) << total << std::endl;
 
+    std::cout << "---------------------" << std::endl;
+
     std::ostringstream oss {};
+    oss << std::setw(10) << std::left << name
+        << std::setw(5) << num
+        << std::setw(10) << total << std::endl;
     std::cout << "ostringstream output: " << oss.str() << std::endl;
+
+    std::cout << "---------------------" << std::endl;
+
+    int value {};
+    std::string entry {};
+    bool done {};
+    do {
+        std::cout << "Please enter an integer: ";
+        std::cin >> entry;
+        std::istringstream validator {entry};
+        if (validator >> value) {
+            std::cout << entry << " is an integer!" << std::endl;
+            done = true;
+        } else {
+            std::cout << entry << " is not an integer..." << std::endl;
+        }
+
+        // discard input buffer
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    } while (done != true);
 
     return 0;
 }
