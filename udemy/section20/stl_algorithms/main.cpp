42e880562a130da277e7f591a6ac09be827f6b95
Michal Moudrý <moudry.michal@live.com>
2024-08-06 17:27:50 +0200

    changes to stl alg

--- udemy/section20/stl_algorithms/main.cpp
+++ udemy/section20/stl_algorithms/main.cpp
@@ -22,14 +22,80 @@ public:
 void find_test() {
     std::cout << "=====================" << std::endl;
     std::vector<int> vec {1, 2, 3, 4, 5};
-    auto loc {std::find(std::begin(vec), std::end(vec), 1)};
+    auto end {std::end(vec)};
+    auto loc {std::find(std::begin(vec), end, 1)};
 
-    if (loc != std::end(vec)) {
-        /* code */
+    if (loc != end) {
+        std::cout << "Found the number: " << *loc << std::endl;
+    } else {
+        std::cout << "Couldn't find the number" << std::endl;
     }
+
+    std::list<Person> players {
+        {"Larry", 18},
+        {"Moe", 20},
+        {"Curly", 21}
+    };
+    auto players_end {players.end()};
+    auto loc1 {
+        std::find(players.begin(), players_end, Person {"Moe", 20})
+    };
+    if (loc1 != players_end) {
+        std::cout << "Found Moe" << std::endl;
+    } else {
+        std::cout << "Moe not found" << std::endl;
+    }
+}
+
+void count_test() {
+    std::cout << "=====================" << std::endl;
+    std::vector<int> vec {1, 2, 3, 4, 5, 1, 2, 1};
+
+    int num {
+        static_cast<int>(std::count(vec.begin(), vec.end(), 1))
+    };
+    std::cout << num << " occurrences found" << std::endl;
+}
+
+void count_if_test() {
+    std::cout << "=====================" << std::endl;
+    std::vector<int> vec {1, 2, 3, 4, 5, 1, 2, 1, 100};
+    auto beginning {vec.begin()};
+    auto end {vec.end()};
+
+    int num = std::count_if(
+        beginning,
+        end,
+        [](int x) { return x % 2 == 0; }
+    );
+    std::cout << num << " even numbers found" << std::endl;
+
+    num = std::count_if(
+        beginning,
+        end,
+        [](int x) { return x % 2 != 0; }
+    );
+    std::cout << num << " odd numbers found" << std::endl;
+
+    num = std::count_if(
+        beginning,
+        end,
+        [](int x) { return x >= 5; }
+    );
+    std::cout << num << " are greater or equal than 5" << std::endl;
+}
+
+void replace_test() {
+
+}
+
+void all_of_test() {
+
 }
 
 int main() {
     find_test();
+    count_test();
+    count_if_test();
     return 0;
 }
