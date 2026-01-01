eee6e772be5fdcd1bfa893be3843e3d55eeb2a33
Michal Moudrý <moudry.michal@live.com>
2026-01-01 21:31:02 +0100

    Finished enums

--- udemy/section23/ScopedEnums/main.cpp
+++ udemy/section23/ScopedEnums/main.cpp
@@ -1,3 +1,84 @@
+#include <iostream>
+#include <type_traits>
+
+enum struct GroceryItem { Milk = 350, Bread = 250, Apple = 132, Orange = 100 };
+
+std::ostream& operator<<(std::ostream& os, const GroceryItem& item) {
+    auto val {std::underlying_type_t<GroceryItem>(item)};
+    switch (item) {
+        case GroceryItem::Milk:
+            os << "Milk";
+            break;
+        case GroceryItem::Bread:
+            os << "Bread";
+            break;
+        case GroceryItem::Apple:
+            os << "Apple";
+            break;
+        case GroceryItem::Orange:
+            os << "Orange";
+            break;
+        default:
+            os << "Invalid item";
+    }
+    os << ": " << val;
+
+    return os;
+}
+
+bool is_valid_grocery_item(GroceryItem item) {
+    switch (item) {
+        case GroceryItem::Milk:
+        case  GroceryItem::Bread:
+        case GroceryItem::Apple:
+        case GroceryItem::Orange:
+            return true;
+        default:
+            return false;
+    }
+}
+
+void display_grocery_list(const std::vector<GroceryItem>& groceryList) {
+    std::cout << "Grocery list" << "\n====================" << std::endl;
+    int invalid_item_count {0};
+    int valid_item_count {0};
+    for (GroceryItem item : groceryList) {
+        std::cout << item << std::endl;
+        if (is_valid_grocery_item(item)) {
+            valid_item_count += 1;
+        }
+        else {
+            invalid_item_count += 1;
+        }
+    }
+
+    std::cout << "====================" << std::endl;
+    std::cout << "Valid items: " << valid_item_count << std::endl;
+    std::cout << "Invalid items: " << invalid_item_count << std::endl;
+    std::cout << "Total: " << valid_item_count + invalid_item_count << std::endl;
+}
+
+void test1() {
+    std::cout << "\nTest 1 ====================" << std::endl;
+    std::vector<GroceryItem> shopping_list {
+        GroceryItem::Apple,
+        GroceryItem::Milk,
+        GroceryItem::Orange
+    };
+    int helicopter {100};
+    shopping_list.emplace_back(GroceryItem(helicopter));
+    shopping_list.emplace_back(GroceryItem(0));
+
+    display_grocery_list(shopping_list);
+}
+
+void test2() {
+
+}
+
 int main() {
+    test1();
+    test2();
+
     return 0;
 }
--- udemy/section23/UnscopedEnums/main.cpp
+++ udemy/section23/UnscopedEnums/main.cpp
@@ -1,3 +1,156 @@
+#include <iostream>
+#include <type_traits>
+#include <vector>
+#include <string>
+
+enum Direction { North, South, East, West };
+
+std::string direction_to_str(Direction dir) {
+    std::string result;
+    switch (dir) {
+        case North:
+            result = "North";
+            break;
+        case South:
+            result = "South";
+            break;
+        case East:
+            result = "East";
+            break;
+        case West:
+            result = "West";
+            break;
+        default:
+            result = "Unknown direction";
+    }
+
+    return result;
+}
+
+void test1() {
+    std::cout << "Test 1 ====================" << std::endl;
+    Direction dir {North};
+
+    std::cout << "Direction: " << dir << std::endl;
+    std::cout << direction_to_str(dir) << std::endl;
+
+    dir = Direction(100);
+    std::cout << "Direction: " << dir << std::endl;
+    std::cout << direction_to_str(dir) << std::endl;
+
+    dir = static_cast<Direction>(100);
+    std::cout << "Direction: " << dir << std::endl;
+    std::cout << direction_to_str(dir) << std::endl;
+}
+
+enum GroceryItem { Milk, Bread, Apple, Orange };
+
+std::ostream& operator<<(std::ostream& os, GroceryItem groceryItem) {
+    switch (groceryItem) {
+        case Milk:
+            os << "Milk";
+            break;
+        case Bread:
+            os << "Bread";
+            break;
+        case Apple:
+            os << "Apple";
+            break;
+        case Orange:
+            os << "Orange";
+            break;
+        default:
+            os << "Unknown grocery item";
+    }
+    return os;
+}
+
+bool is_valid_grocery_item(GroceryItem item) {
+    switch (item) {
+        case Milk:
+        case Bread:
+        case Apple:
+        case Orange:
+            return true;
+        default:
+            return false;
+    }
+}
+
+void display_grocery_list(const std::vector<GroceryItem>& groceryList) {
+    std::cout << "Grocery list" << "\n====================" << std::endl;
+    int invalid_item_count {0};
+    int valid_item_count {0};
+    for (GroceryItem item : groceryList) {
+        std::cout << item << std::endl;
+        if (is_valid_grocery_item(item)) {
+            valid_item_count += 1;
+        }
+        else {
+            invalid_item_count += 1;
+        }
+    }
+
+    std::cout << "====================" << std::endl;
+    std::cout << "Valid items: " << valid_item_count << std::endl;
+    std::cout << "Invalid items: " << invalid_item_count << std::endl;
+    std::cout << "Total: " << valid_item_count + invalid_item_count << std::endl;
+}
+
+void test2() {
+    std::cout << "\nTest 2 ====================" << std::endl;
+    std::vector<GroceryItem> shopping_list {Apple, Apple, Milk, Orange};
+    int helicopter {100};
+    shopping_list.emplace_back(GroceryItem(helicopter));
+    shopping_list.emplace_back(GroceryItem(0));
+
+    display_grocery_list(shopping_list);
+}
+
+enum State { EngineFailure, InclementWeather, Nominal, Unknown };
+enum Sequence { Abort, Hold, Launch };
+
+std::istream& operator>>(std::istream& is, State& state) {
+    std::underlying_type_t<Sequence> user_input;
+    is >> user_input;
+    switch (user_input) {
+        case EngineFailure:
+        case InclementWeather:
+        case Unknown:
+        case Nominal:
+            state = State(user_input);
+            break;
+        default:
+            std::cout << "User input is not a valid launch state" << std::endl;
+            state = Unknown;
+    }
+
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Sequence& seq) {
+    switch (seq) {
+        case Abort:
+            os << "Abort";
+            break;
+        case Hold:
+            os << "Hold";
+            break;
+        case Launch:
+            os << "Launch";
+            break;
+    }
+
+    return os;
+}
+
+void test3() {
+
+}
+
 int main() {
+    test1();
+    test2();
+
     return 0;
 }
